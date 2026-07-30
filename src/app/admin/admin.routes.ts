d1520d698f9ef5928b12dd33b62172738a2c592c
Chris <christophermedinabarroso@gmail.com>
2026-07-29 20:16:41 -0400

    routes

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from "@angular/router";
 
 export const AdminRoutes: Routes = [
-
+    {
+        path: '',
+        loadComponent: () => import('./dashboard/dashboard.component').then((c) => c.DashboardComponent),
+        title: 'Admin Dashboard'
+    }
 ]
