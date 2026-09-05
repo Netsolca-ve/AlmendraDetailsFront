02f02032c7ac772f625c38a8077114ca720b23bd
Chris <christophermedinabarroso@gmail.com>
2026-09-05 11:57:51 -0400

    home

--- src/app/shop/home/home.component.ts
+++ src/app/shop/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NgZorroModule } from '../../shared/ng-zorro/ng-zorro.module';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    NgZorroModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
